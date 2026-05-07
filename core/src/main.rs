ffd0499eecbf2d30a2e951b8dab1bb71004d97da
horizon <rekka728@gmail.com>
2026-05-08 00:15:41 +0900

    [update] pointer

--- core/src/main.rs
+++ core/src/main.rs
@@ -6,5 +6,14 @@ nun::entry!(main);
 fn main(init_info: &nun::InitInfo) {
     nun::println!("Hello, world!");
 
+    nun::println!(
+        "version: {}.{}.{}-{}+{}",
+        init_info.kernel_major_version,
+        init_info.kernel_minor_version,
+        init_info.kernel_patch_version,
+        init_info.get_pre_release_string(),
+        init_info.get_build_metadata_string(),
+    );
+
     loop {}
 }
